90ae52ae21c5b23bf2277dedc6bba4a4de19fc56
Lord 🧃 <hostcollab703@gmail.com>
2026-02-20 21:24:39 +0100

    Create ecosystem.config.cjs file for Kermv1-session

---
+++ ecosystem.config.cjs
@@ -0,0 +1,1 @@
+module.exports = { apps: [{ name: 'kerm-bot', script: './index.js', type: 'module', instances: 1, exec_mode: 'fork', max_restarts: 10, min_uptime: 10000, restart_delay: 5000, error_file: './logs/err.log', out_file: './logs/out.log', log_date_format: 'YYYY-MM-DD HH:mm:ss Z', env: { NODE_ENV: 'production' } }] }
